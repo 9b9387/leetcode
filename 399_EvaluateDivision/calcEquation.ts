080b1522e44f17cc88987a5db4185b13ccf342d7
9b9387 <9b9387@gmail.com>
2021-01-09 16:38:01 +0800

    update 399

--- 399_EvaluateDivision/calcEquation.ts
+++ 399_EvaluateDivision/calcEquation.ts
@@ -1,5 +1,22 @@
+import { UnionFind } from "../common/DisjoinSet/UnionFInd2";
+
 function calcEquation(equations: string[][], values: number[], queries: string[][]): number[] {
-    return [];
+    let set: UnionFind = new UnionFind(equations.length * 2);
+
+    for (let i = 0; i < equations.length; i++) {
+        var x = equations[i][0].charCodeAt(0);
+        var y = equations[i][1].charCodeAt(1);
+        var w = values[i];
+        set.union(x, y, w);        
+    }
+
+    let result = []
+    for (let i = 0; i < queries.length; i++) {
+        var x = equations[i][0].charCodeAt(0);
+        var y = equations[i][1].charCodeAt(1);
+        result.push(set.isConnected(x, y))
+    }
+    return result;
 };
 
 export { calcEquation }
---
+++ common/DisjoinSet/UnionFind2.ts
@@ -0,0 +1,44 @@
+class UnionFind {
+
+    private parent: number[] = [];
+    private weight: number[] = [];
+
+    constructor(count: number) {
+        this.parent = new Array(count);
+        this.weight = new Array(count);
+        for (let i = 0; i < count; i++) {
+            this.parent[i] = i;
+            this.weight[i] = 1;
+        }
+    }
+
+    public union(a: number, b: number, w: number) {
+        let rootA = this.find(a);
+        let rootB = this.find(b);
+        if (rootA == rootB) {
+            this.parent[a] = this.parent[b];
+            this.weight[a] = w * this.weight[b] / this.weight[a]
+            return;
+        }
+    }
+
+    public find(v: number): number {
+        if (this.parent[v] == v) {
+            return v;
+        }
+        else {
+            return this.find(this.parent[v]);
+        }
+    }
+
+    public isConnected(a: number, b: number): number {
+        let rootA = this.find(a);
+        let rootB = this.find(b);
+        if (rootA == rootB) {
+            return this.parent[a] / this.parent[b];
+        }
+        return -1;
+    }
+}
+
+export { UnionFind }
