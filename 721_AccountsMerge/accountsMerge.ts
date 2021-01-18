572a31ca44461d2fca579d1450623f5cdabcd794
9b9387 <9b9387@gmail.com>
2021-01-18 16:44:48 +0800

    update 721 comment

--- 721_AccountsMerge/accountsMerge.ts
+++ 721_AccountsMerge/accountsMerge.ts
@@ -4,6 +4,9 @@ function accountsMerge(accounts: string[][]): string[][] {
     const emailToIndex = new Map();
     const emailToName = new Map();
     let emailsCount = 0;
+    // 整理数据
+    // 每个email对应一个index 保存到emailToIndex
+    // 每个email对应一个name 保存到emailToName
     for (const account of accounts) {
         const name = account[0];
         const size = account.length;
@@ -16,6 +19,8 @@ function accountsMerge(accounts: string[][]): string[][] {
         }
     }
 
+    // 建立并查集
+    // 相邻的email用union方法合并
     const uf = new UnionFind(emailsCount);
     for (const account of accounts) {
         const firstEmail = account[1];
@@ -28,6 +33,7 @@ function accountsMerge(accounts: string[][]): string[][] {
         }
     }
 
+    // 在并查集内，查找所有索引相同的email 保存到indexToEmails中
     const indexToEmails = new Map();
     for (const email of emailToIndex.keys()) {
         const index = uf.find(emailToIndex.get(email));
@@ -35,6 +41,7 @@ function accountsMerge(accounts: string[][]): string[][] {
         account.push(email);
         indexToEmails.set(index, account);
     }
+    // 整理结果
     const merged = [];
     for (const emails of indexToEmails.values()) {
         emails.sort();
