3218e40c92659f130d14cd24de683bf97c5be92c
9b9387 <9b9387@gmail.com>
2021-03-29 16:38:41 +0800

    update 1622

--- 1622_FancySequence/Fancy.ts
+++ 1622_FancySequence/Fancy.ts
@@ -29,6 +29,60 @@ class Fancy {
         return this.sequence[idx] ?? -1;
     }
 }
+
+// 第二种解法：
+// var Fancy = function() {
+//     this.ary = [] // 新建空数组
+//     this.hint = [] // 存储修改数组的记录
+//     this.getAry = [] // 存储重复取值
+//     this.pow = Math.pow(10, 9)+7
+// };
+
+// /** 
+//  * @param {number} val
+//  * @return {void}
+//  */
+// Fancy.prototype.append = function(val) {
+//     this.ary.push(val)
+// };
+
+// /** 
+//  * @param {number} inc
+//  * @return {void}
+//  */
+// Fancy.prototype.addAll = function(inc) {
+//     this.hint.push(['+', inc, this.ary.length])
+// };
+
+// /** 
+//  * @param {number} m
+//  * @return {void}
+//  */
+// Fancy.prototype.multAll = function(m) {
+//     this.hint.push(['*', m, this.ary.length])
+// };
+
+// /** 
+//  * @param {number} idx
+//  * @return {number}
+//  */
+// Fancy.prototype.getIndex = function(idx) {
+//     if(idx >= this.ary.length) return -1
+//     if(this.ary.length == this.getAry[1] && this.getAry[0] == this.hint.length && this.getAry[3] == idx) return this.getAry[2]
+//     let n = this.ary[idx]
+//     for(let [h, num, l] of this.hint) {
+//         if(idx >= l) continue
+//         if(h == '+') {
+//             n += num
+//         }else{
+//             n *= num
+//         }
+//         n %= this.pow
+//     }
+//     this.getAry = [this.hint.length, this.ary.length, n, idx]
+//     return n
+// };
+
 /**
  * Your Fancy object will be instantiated and called as such:
  * var obj = new Fancy()
